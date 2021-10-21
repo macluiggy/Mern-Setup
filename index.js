31e1514affbcaca7209c11fc01a852a10c227263
macluiggy <ferrinluiggy@gmail.com>
2021-10-21 10:24:00 -0500

    ignoring node_modules

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import logger from "morgan"
 import cors from "cors"
 
 const {pathname: root} = new URL('../frontend/build', import.meta.url)
-
+const {pathname: root2} = new URL('../frontend/build/index.js', import.meta.url)
+console.log(root)
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,11 +17,11 @@ app.get("/api/test", (req, res) => {
   res.send("test");
 });
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(root));
 
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "./frontend/build/index.html"),
+    root2,
     function (err) {
       if (err) {
         res.status(500).send(err);
@@ -32,4 +33,4 @@ app.get("*", function (_, res) {
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server Running on port ${port}`));
 
-module.exports = app;
+export default app;
