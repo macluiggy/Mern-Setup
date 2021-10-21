eec654450325c558357d986ff0ab7f4b34291f07
macluiggy <ferrinluiggy@gmail.com>
2021-10-21 09:45:48 -0500

    updated

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+const app = express();
+
+import path from "path"
+import logger from "morgan"
+import cors from "cors"
+
+const {pathname: root} = new URL('../frontend/build', import.meta.url)
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/api/test", (req, res) => {
+  res.send("test");
+});
+
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server Running on port ${port}`));
+
+module.exports = app;
